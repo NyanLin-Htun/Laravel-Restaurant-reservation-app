b1cda2bddca3d9cf20709394cfe110103404fcc1
NyanLin-Htun <nyanlinhtun147000@gmail.com>
2022-05-30 01:41:33 +0630

    Menu and Table Page CRUD

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TableStoreRequest;
 use App\Models\Table;
 use Illuminate\Http\Request;
 
@@ -15,8 +16,9 @@ class TableController extends Controller
      */
     public function index()
     {
-        $tables = Table::all();
-        return view('admin.tables.index' , compact('tables'));
+        return view('admin.tables.index' , [
+            'tables' => Table::all()
+        ]);
     }
 
     /**
@@ -35,9 +37,16 @@ class TableController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TableStoreRequest $request)
     {
-        //
+        Table::create([
+            'name' => $request->name,
+            'guest_number' => $request->guest_number,
+            'status' => $request->status,
+            'location' => $request->location
+        ]);
+
+        return view('admin.tables.index');
     }
 
     /**
@@ -57,9 +66,9 @@ class TableController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Table $table)
     {
-        //
+        return view('admin.tables.edit', compact('table'));
     }
 
     /**
@@ -69,9 +78,10 @@ class TableController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TableStoreRequest $request, Table $table)
     {
-        //
+        $table->update($request->validated());
+        return view('admin.tables.index');
     }
 
     /**
@@ -80,8 +90,10 @@ class TableController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Table $table)
     {
-        //
+        $table->delete();
+        return view('admin.tables.index');
+
     }
 }
--- resources/views/admin/menus/edit.blade.php
+++ resources/views/admin/menus/edit.blade.php
@@ -12,30 +12,34 @@
             </div>
             <div class="m-2 p-2 bg-slate-100 rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                    <form method="POST" action="{{ route('admin.menus.store') }}" enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('admin.menus.update' , $menu->id) }}" enctype="multipart/form-data">
                       @csrf
+                      @method('PUT')
                       <div class="sm:col-span-6">
                         <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
                         <div class="mt-1">
-                          <input type="text" id="name" name="name" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                          <input type="text" id="name" name="name" value="{{ $menu->name }}" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                         </div>
                       </div>
                       <div class="sm:col-span-6">
                         <label for="image" class="block text-sm font-medium text-gray-700">Image </label>
                         <div class="mt-1">
+                            <div>
+                                <img class="w-32 h-32" src="{{ Storage::url($menu->image) }}">
+                            </div>
                           <input type="file" id="image" name="image" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                         </div>
                       </div>
                       <div class="sm:col-span-6">
                         <label for="price" class="block text-sm font-medium text-gray-700"> Price </label>
                         <div class="mt-1">
-                          <input type="number" min="0.00" max="10000.00" step="0.01" id="price" name="price" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                          <input type="number" min="0.00" max="10000.00" step="0.01" id="price" name="price" value="{{ $menu->price }}" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                         </div>
                       </div>
                       <div class="sm:col-span-6 pt-5">
                         <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
                         <div class="mt-1">
-                          <textarea id="description" name="description" rows="3" class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                          <textarea id="description" name="description" rows="3" class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">{{ $menu->description }}</textarea>
                         </div>
                       </div>
                       <div class="sm:col-span-6 pt-5">
@@ -43,13 +47,13 @@
                         <div class="mt-1">
                           <select id="categories" name="categories[]" class="form-multiselect block w-full mt-1" multiple>
                             @foreach ($categories as $category)
-                              <option value="{{ $category->id }}">{{ $category->name }}</option>
+                              <option value="{{ $category->id }}" @selected($menu->categories->contains($category))>{{ $category->name }}</option>
                             @endforeach
                           </select>  
                         </div>
                       </div>
                       <div class="mt-4 p-4">
-                        <button class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white" type="submit">Store</button>
+                        <button class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white" type="submit">Update</button>
                       </div>
                     </form>
                   </div>
